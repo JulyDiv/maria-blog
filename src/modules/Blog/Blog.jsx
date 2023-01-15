85957ee65db853be57a6801680009d54c27974a6
JulyDiv <july_developer@list.ru>
2023-01-15 15:42:00 +0700

    исправила отображение кнопок пагинации + адаптив

--- src/modules/Blog/Blog.jsx
+++ src/modules/Blog/Blog.jsx
@@ -13,44 +13,6 @@ export const Blog = ({
   isLoading,
   setIsLoading,
 }) => {
-
-  const [btnBottom, setBtnBottom] = useState(true);
-
-  const reversePost = posts.slice(0).reverse();
-
-  const [currentPage, setCurrentPage] = useState(1);
-  const [postPerPage, setPostPerPage] = useState(2);
-
-  const lastPostIndex = currentPage * postPerPage;
-  const firstPostIndex = lastPostIndex - postPerPage;
-  const currentPost = reversePost.slice(firstPostIndex, lastPostIndex);
-
-  const onPagination = (pageNumber) => {
-    setCurrentPage(pageNumber);
-  };
-
-  const nextPage = () => {
-    setCurrentPage((prev) => prev + 1);
-  };
-  const prevPage = () => {
-    setCurrentPage((prev) => prev - 1);
-  };
-
-  const totalPosts = posts.length;
-  const postCount = Math.ceil(totalPosts / postPerPage);
-  const pageNumbers = [];
-  for (let i = 1; i <= postCount; i++) {
-    pageNumbers.push(i);
-    // if (pageNumbers.length > 4) {
-    //   pageNumbers.slice(0, 4)
-    // } else {
-    //   pageNumbers.push(i);
-    // }
-  }
-
-  //console.log(pageNumbers.length);
-  //console.log(postCount);
-
   const getData = async () => {
     setIsLoading(true);
     return await axios
@@ -68,9 +30,49 @@ export const Blog = ({
     getData();
   }, []);
 
-  // if (isLoading) {
-  //   return <h1 style={{ display: "flex", justifyContent: "center" }}>Загрузка постов...</h1>;
-  // }
+  const reversePost = posts.slice(0).reverse();
+  const portionSize = 3;
+
+  const [currentPage, setCurrentPage] = useState(1);
+  const [postPerPage, setPostPerPage] = useState(2);
+
+  const lastPostIndex = currentPage * postPerPage;
+  const firstPostIndex = lastPostIndex - postPerPage;
+  const currentPost = reversePost.slice(firstPostIndex, lastPostIndex);
+
+
+  const nextPage = () => {
+    //setCurrentPage((prev) => prev + 1);
+    setPortionNumber(portionNumber + 1);
+  };
+  const prevPage = () => {
+    //setCurrentPage((prev) => prev - 1);
+    setPortionNumber(portionNumber - 1);
+  };
+
+  const totalPosts = posts.length;
+  const postCount = Math.ceil(totalPosts / postPerPage);
+  const pageNumbers = [];
+  for (let i = 1; i <= postCount; i++) {
+    pageNumbers.push(i);
+  }
+
+  const portionCount = Math.ceil(postCount / portionSize);
+  const [portionNumber, setPortionNumber] = useState(1);
+  const leftPortionPageNumber = (portionNumber - 1) * portionSize + 1;
+  const rightPortionPageNumber = portionNumber * portionSize;
+
+    const onPagination = (pageNumber) => {
+      setCurrentPage(pageNumber);
+      console.log(pageNumber);
+    };
+
+  //console.log(pageNumber);
+
+  // useEffect(
+  //   () => setPortionNumber(Math.ceil(currentPage / portionSize)),
+  //   [currentPage]
+  // );
 
   return (
     <>
@@ -82,7 +84,7 @@ export const Blog = ({
         ) : (
           <>
             <div className={`${styles.block} ${styles.block_top}`}>
-              {currentPage > 1 && (
+              {portionNumber > 1 && (
                 <button className={styles.button} onClick={() => prevPage()}>
                   Предыдущие
                 </button>
@@ -91,13 +93,34 @@ export const Blog = ({
                 postPerPage={postPerPage}
                 totalPosts={totalPosts}
                 onPagination={onPagination}
+                currentPage={currentPage}
+                pageNumbers={pageNumbers}
+                leftPortionPageNumber={leftPortionPageNumber}
+                rightPortionPageNumber={rightPortionPageNumber}
+                portionNumber={portionNumber}
+              />
+              {portionCount > portionNumber && (
+                <button className={styles.button} onClick={() => nextPage()}>
+                  Далее
+                </button>
+              )}
+              {/* {currentPage > 1 && (
+                <button className={styles.button} onClick={() => prevPage()}>
+                  Предыдущие
+                </button>
+              )}
+              <Pagination
+                postPerPage={postPerPage}
+                totalPosts={totalPosts}
+                onPagination={onPagination}
+                currentPage={currentPage}
                 pageNumbers={pageNumbers}
               />
               {postCount > currentPage && (
                 <button className={styles.button} onClick={() => nextPage()}>
                   Далее
                 </button>
-              )}
+              )} */}
             </div>
             {currentPost.map((post, id) => (
               <Posts
@@ -111,12 +134,21 @@ export const Blog = ({
                 setIsLoading={setIsLoading}
               />
             ))}
+            {/* {currentPost.map((post, id) => (
+              <Posts
+                key={id}
+                post={post}
+                isLogged={isLogged}
+                setPosts={setPosts}
+                posts={posts}
+                getData={getData}
+                isLoading={isLoading}
+                setIsLoading={setIsLoading}
+              />
+            ))} */}
             <div className={`${styles.block} ${styles.block_bottom}`}>
-              {currentPage > 1 && (
-                <button
-                  className={styles.button}
-                  onClick={() => prevPage()}
-                >
+              {/* {currentPage > 1 && (
+                <button className={styles.button} onClick={() => prevPage()}>
                   Предыдущие
                 </button>
               )}
@@ -128,13 +160,10 @@ export const Blog = ({
                 btnBottom={btnBottom}
               />
               {postCount > currentPage && (
-                <button
-                  className={styles.button}
-                  onClick={() => nextPage()}
-                >
+                <button className={styles.button} onClick={() => nextPage()}>
                   Далее
                 </button>
-              )}
+              )} */}
             </div>
           </>
         )}
--- src/modules/Pagination/Pagination.jsx
+++ src/modules/Pagination/Pagination.jsx
@@ -2,19 +2,48 @@ import Link from "next/link";
 import React from "react";
 import styles from "./Pagination.module.sass";
 
-export const Pagination = ({ btnBottom, onPagination, pageNumbers }) => {
-  //const pageNumbers = [];
-  // for (let i = 1; i <= Math.ceil(totalPosts / postPerPage); i++) {
-  //   pageNumbers.push(i);
-  // }
+export const Pagination = ({
+  btnBottom,
+  onPagination,
+  pageNumbers,
+  currentPage,
+  leftPortionPageNumber,
+  rightPortionPageNumber,
+  portionNumber,
+}) => {
+
   return (
     <>
       <div className={styles.pagination}>
         <ul className={styles.list}>
+          {pageNumbers
+            .filter(
+              (number) =>
+                number >= leftPortionPageNumber &&
+                number <= rightPortionPageNumber
+            )
+            .map((number) => (
+              <li key={number} className={styles.item}>
+                <button
+                  className={
+                    currentPage === number
+                      ? `${styles.button} ${styles.button_active}`
+                      : `${styles.button}`
+                  }
+                  onClick={(e) => onPagination(number)}
+                >
+                  {number}
+                </button>
+              </li>
+            ))}
+        </ul>
+      </div>
+      {/* <div className={styles.pagination}>
+        <ul className={styles.list}>
           {pageNumbers.map((number) => (
             <li key={number} className={styles.item}>
               <button
-                className={`${styles.button}`}
+                className={currentPage === number ? `${styles.button} ${styles.button_active}` : `${styles.button}`}
                 onClick={() => onPagination(number)}
               >
                 {number}
@@ -22,7 +51,7 @@ export const Pagination = ({ btnBottom, onPagination, pageNumbers }) => {
             </li>
           ))}
         </ul>
-      </div>
+      </div> */}
     </>
   );
 };
